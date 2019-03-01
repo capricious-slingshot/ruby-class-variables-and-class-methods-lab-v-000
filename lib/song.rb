6b4d6b163528d7118ca100482e1e3ea27538a003
Carden Z <ephemere@protonmail.com>
2019-03-01 00:20:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,4 +2,13 @@ class Song
   @@count = []
 	@@artists = []
 	@@genres = []
+
+	attr_accessor :name, :artist, :genres
+
+
+	def initialize (name, artist, genre)
+    @name   = name
+		@artist = artist
+		@genre  = genre
+	end
 end
